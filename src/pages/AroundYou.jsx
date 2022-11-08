2a9476ba4d0bbe2f1a17fe42e7ee5291ed6c7aff
oletilwe12 <loatemophuting@gmail.com>
2022-11-08 12:13:22 +0200

    around you

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -12,7 +12,8 @@ const CountryTracks = () => {
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
-    axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_YaVbcIzwXteGOSztz4OGLB11IoDoN`)
+    axios
+      .get(`https://geo.ipify.org/api/v2/country?apiKey=at_YaVbcIzwXteGOSztz4OGLB11IoDoN`)
       .then((res) => setCountry(res?.data?.location.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
@@ -24,7 +25,7 @@ const CountryTracks = () => {
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around You <span className="font-black">{country}</span></h2>
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you <span className="font-black">{country}</span></h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
